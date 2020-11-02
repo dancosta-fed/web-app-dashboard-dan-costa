1ff19df2923752e94db9781146d14a528fe7036b
Dan Costa <dancosta.id@gmail.com>
2020-11-02 18:59:17 +1300

    Really good progress

--- js/script.js
+++ js/script.js
@@ -20,7 +20,9 @@ alertBanner.addEventListener('click', e => {
 
 // === CHART WIDGETS === //
 
-// outline
+// ========== Traffic Canvas ========== //
+
+// outline Traffic Canvas
 const trafficCanvas = document.getElementById('traffic-chart');
 let trafficData = {
     labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
@@ -56,5 +58,75 @@ let trafficChart = new Chart(trafficCanvas, {
     options: trafficOptions,
 });
 
+// ========== Bar Graph ========== //
 
-console.log();
+// Outline bar graph
+const dailyCanvas = document.getElementById('daily-chart');
+let dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477bf',
+        borderWidth: 1,
+    }]
+};
+
+// Chart options
+let dailyOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+};
+
+// The chart itself
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData, 
+    options: dailyOptions
+});
+
+// ========== Doughnut Chart ========== //
+
+// Outline Doughnut Chart
+const mobileCanvas = document.getElementById('mobile-chart');
+let mobileData = {
+    labels: ['Desktop', 'Tablet', 'Phones'],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        backgroundColor: [
+            '#7477bf',
+            '#78CF82',
+            '#51b6c8'
+        ],
+        borderWidth: 0,
+    }]
+};
+
+// chart options
+let mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+};
+
+// The chart itself
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData, 
+    options: mobileOptions
+});
+
+console.log(mobileChart);
