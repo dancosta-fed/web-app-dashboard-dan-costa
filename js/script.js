dffa1a0f35cfdf5a633b01efd7f4934d4bdb5125
Dan Costa <dancosta.id@gmail.com>
2020-11-08 09:20:47 +1300

    Working on the charts on Js

--- js/script.js
+++ js/script.js
@@ -22,12 +22,47 @@ alertBanner.addEventListener('click', e => {
 
 // ========== Traffic Canvas ========== //
 
-// outline Traffic Canvas
+// outline Traffic Canvas | WEEKLY
 const trafficCanvas = document.getElementById('traffic-chart');
+
 let trafficData = {
-    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+    labels: ['16-32', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: '#e2e3f6',
+        pointBackgroundColor: '#fff',
+        borderWidth: 1,
+    }]
+};
+
+
+// HOURLY
+let trafficDataHourly = {
+    labels: ['13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'],
+    datasets: [{
+        data: [100, 450, 250, 200, 600, 150, 500, 300, 250, 550, 700, 400],
+        backgroundColor: '#e2e3f6',
+        pointBackgroundColor: '#fff',
+        borderWidth: 1,
+    }]
+};
+
+// DAILY
+let trafficDataDaily = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [{
+        data: [10, 40, 55, 30, 50, 20, 45, 50, 15],
+        backgroundColor: '#e2e3f6',
+        pointBackgroundColor: '#fff',
+        borderWidth: 1,
+    }]
+};
+
+// MONTHLY
+let trafficDataMonthly = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [{
+        data: [50, 30, 35, 65, 80, 40, 70, 30, 25, 55, 70, 40, 50, 90],
         backgroundColor: '#e2e3f6',
         pointBackgroundColor: '#fff',
         borderWidth: 1,
@@ -54,9 +89,13 @@ let trafficOptions = {
 // the chart itself
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
-    data: trafficData, 
+    data: trafficData, trafficDataHourly, trafficDataDaily, trafficDataMonthly,  
     options: trafficOptions,
 });
+
+
+console.log();
+
 
 // ========== Bar Graph ========== //
 
@@ -129,4 +168,24 @@ let mobileChart = new Chart(mobileCanvas, {
     options: mobileOptions
 });
 
-console.log(mobileChart);
+// ========== Messaging ========== //
+
+    //Storing the form fields
+    const user = document.getElementById('userField');
+    const message = document.getElementById('messageField');
+    const send = document.getElementById('btn');
+
+    //Click event listener on send
+    send.addEventListener('click', () => {
+
+        // conditional to make sure the user and message fields are not empty
+        if (user.value === '' && message.value === '') {
+            alert('Please, fill out user and message fields before sending');
+        } else if (user.value === '') {
+            alert('Please fill out the user field before sending');
+        } else if(message.value === '') {
+            alert('Please fill out the message field before sending');
+        } else {
+            alert(`Message has been succesfully sent to ${user.value}`);
+        }
+    });
